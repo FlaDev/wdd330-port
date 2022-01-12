d9495b12ad1525a01a542c0b80cff5ff41146147
FlaDev <flaprumucena@byui.edu>
2022-01-11 21:45:51 -0700

    Adding notes after SitePoint account was enable

--- js/main.js
+++ js/main.js
@@ -5,6 +5,22 @@ const links = [
     }
 ]
 
+const week1 = [
+    {text: "Always Design for Small devices first and think corectly from the beginning"},
+    {text: "Mobile users are the majority with difference sizes and resolutions"},
+    {text: "Understand users is the first step to build good UX pages"},
+    {text: "Poor design causes companies to lose business"},
+    {text: "It's important to reduce the cognitive user processing by creating fluid forms"}
+    
+]
+
+const week2 = [
+    {text: "Always Design for Small devices first and think corectly from the beginning"},
+    {text: "Mobile users are the majority with difference sizes and resolutions"},
+]
+
+const weeks = [week1, week2]
+
 function getMenu() {
     let list = document.getElementById("menu-list");    
     links.forEach(function (item) {
@@ -13,6 +29,15 @@ function getMenu() {
         a.textContent = item.label;
         a.setAttribute('href', item.url);
         li.appendChild(a)
+        list.appendChild(li);
+    });
+}
+
+function generateNotes(elementID, weekIndex){
+    let list = document.getElementById(elementID);    
+    weeks[weekIndex].forEach(function (item) {
+        let li = document.createElement('li');        
+        li.textContent = item.text;
         list.appendChild(li);
     });
 }
