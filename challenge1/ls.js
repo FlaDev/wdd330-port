a6f9a1b1d510216e0a14033136db8c048e653308
FlaDev <prumucena@gmail.com>
2022-02-12 01:07:33 -0700

    fixed a bunch of issues with the app

--- challenge1/ls.js
+++ challenge1/ls.js
@@ -22,14 +22,14 @@ class Storage {
         };
         // console.log('Adding item to storage --> ' + contentObj.id);
         this.storage.setItem(contentID, JSON.stringify(contentObj));
-        return contentID;
+        return contentObj;
     }
 
-    updateItem(contentID, content) {
+    updateItem(contentID, content, completed) {
         let contentObj = {
             id: contentID,
             content: content,
-            completed: false
+            completed: completed
         };
         this.storage.removeItem(contentID);
         this.storage.setItem(contentID, JSON.stringify(contentObj));
@@ -42,114 +42,6 @@ class Storage {
         console.log("Removed object id: " + id);
 
     }
-
-
-
-    // #################
-    /**
-        * Receives all tasks that are in the local storage and load them to the screen
-        * @param {*} storedTasks 
-        */
-    loadAllTasksFromStorageToPage() {
-        let storedTasks = this.loadStorageContent();
-        console.log('local storage tasks size: ' + storedTasks.length);
-        let tasksDiv = document.querySelector("#tasks");
-
-        storedTasks.forEach(element => {
-            let content = JSON.parse(element);
-            let taskDiv = this.#createHtmlElementTask(content);
-            let checkbox = this.#createHtmlElementCheckBox();
-            taskDiv.appendChild(checkbox);
-            let contentDiv = this.#createHtmlElementContent();
-            taskDiv.appendChild(contentDiv);
-            let contentInput = this.#createHtmlElementContentInput(content);
-            contentDiv.appendChild(contentInput);
-            let buttonsDiv = this.#createHtmlElmentButtons(contentInput, taskDiv, tasksDiv);
-            // add input with task value into content div
-            taskDiv.appendChild(buttonsDiv);
-            tasksDiv.appendChild(taskDiv);
-        });
-    }
-
-    #createHtmlElementTask(jsonContent) {
-
-        const divTask = document.createElement('div');
-        divTask.id = jsonContent.id;
-        divTask.classList.add('task');
-        return divTask;
-    }
-
-    #createHtmlElementCheckBox() {
-        const task_input_check_el = document.createElement('input');
-        task_input_check_el.classList.add('cbox');
-        task_input_check_el.type = 'checkbox';
-        task_input_check_el.value = 'checkbox';
-        return task_input_check_el;
-    }
-
-    #createHtmlElementContentInput(jsonContent) {
-        // create input element with task
-        const task_input_el = document.createElement('input');
-        task_input_el.classList.add('text');
-        task_input_el.type = 'text';
-        task_input_el.name = 'todoItem';
-        task_input_el.id = 'todoItem';
-        task_input_el.value = jsonContent.content;
-        task_input_el.setAttribute('readonly', 'readonly');
-        return task_input_el;
-    }
-
-    #createHtmlElementContent() {
-        // create div for content
-        const task_content_el = document.createElement('div');
-        task_content_el.classList.add('content');
-        return task_content_el;
-    }
-
-    #createHtmlElmentButtons(task_input_el, divTask, divTasks) {
-        // create actions (buttons div)
-        const task_actions_el = document.createElement('div');
-        task_actions_el.classList.add('actions');
-        // edit button
-        const task_edit_el = document.createElement('button');
-        task_edit_el.classList.add('edit');
-        task_edit_el.innerText = 'Edit';
-        // Delete button
-        const task_delete_el = document.createElement('button');
-        task_delete_el.classList.add('delete');
-        task_delete_el.innerText = 'Delete';
-        // add to div with buttons
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        divTask.appendChild(task_actions_el);
-		divTasks.appendChild(divTask);
-
-        // Create event listener
-        task_edit_el.addEventListener('click', (e) => {
-            if (task_edit_el.innerText.toLowerCase() == "edit") {
-                task_edit_el.innerText = "Save";
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-            } else {
-                task_edit_el.innerText = "Edit";
-                task_input_el.setAttribute("readonly", "readonly");
-                this.updateItem(divTask.id, task_input_el.value);
-            }
-        });
-        task_delete_el.addEventListener('click', (e) => {
-            this.removeFromStorage(divTask.id);
-            divTasks.removeChild(divTask);
-        });
-
-        return task_actions_el;
-    }
-
-    // #################
-
-
-
-
 
 }
 
--- challenge1/todos.js
+++ challenge1/todos.js
@@ -1,6 +1,6 @@
 
-import {util} from './utilities.js';
-import {ls} from './ls.js';
+import { util } from './utilities.js';
+import { ls } from './ls.js';
 /**
  * 2/10/22
  * Working: local storage save, update and delete 
@@ -10,117 +10,157 @@ import {ls} from './ls.js';
  * Working: show data as HTML document 
  * 
  * TODO: sort the board with ALL, Active and Completed.
- * TODO: ISSUE strike stoped working. Fix: add the class in the javascript code.
+ * FIXED: ISSUE strike stoped working. Fix: add the class in the javascript code.
  * TODO: implement counter of the tasks
  * 
  */
- let divTasks = "";
 
 window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
-	divTasks = document.querySelector("#tasks");
+	const divTasks = document.querySelector("#tasks");
 	const btn_all = document.querySelector("#btn_all");
 	
-	// const storageContent = storage.loadStorageContent();
-	displayAllTasksFromStorage();
-	
-
+	displayAllTasksFromStorage(divTasks);
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
-
 		const task = input.value;
-		const contentID = ls.addIntoStorage(task, false);
-		//CREATE TASK
-		const divTask = document.createElement('div');
-
-		//adds the taskID to the div
-		divTask.id = contentID;
-		divTask.classList.add('task');
-
-		// create check box
-		const task_input_check_el = document.createElement('input');
-		task_input_check_el.classList.add('cbox');
-		task_input_check_el.type = 'checkbox';
-		task_input_check_el.value = 'checkbox';
-		divTask.appendChild(task_input_check_el);
-
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
-
-		divTask.appendChild(task_content_el);
-
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.name = 'todoItem';
-		task_input_el.id = 'todoItem';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
-
-		task_content_el.appendChild(task_input_el);
-
-		// create actions (buttons)
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		divTask.appendChild(task_actions_el);
-		divTasks.appendChild(divTask);
-		input.value = '';
-
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-				storage.updateItem(divTask.id, task_input_el.value);
-			}
-		});
-
-		task_delete_el.addEventListener('click', (e) => {
-			storage.removeFromStorage(divTask.id);
-			divTasks.removeChild(divTask);
-		});
-
-		task_input_check_el.addEventListener('change', (e) => {
-			if (task_input_check_el.checked){
-				task_input_el.classList.add('strike');
-			} else {
-				task_input_el.removeAttribute('class');
-				task_input_el.classList.add('text');
-			}
-
-		});
-	
-
+		const contentObj = ls.addIntoStorage(task, false);
+		createSingleTask(contentObj);
 	});
-
-	btn_all.addEventListener( 'click', (e) => {
-			e.preventDefault();
-			displayAllTasksFromStorage();
-		});
 
 });
 
-function displayAllTasksFromStorage(){
-	//clean page first
-	util.removeAllChildNodes(divTasks);
-	//Load tasks to the screen
-	ls.loadAllTasksFromStorageToPage();
+
+function createSingleTask(contentObj) {
+	console.log('Creating single task: ' + contentObj.content);
+	let tasksDiv = document.querySelector("#tasks");
+
+	let taskDiv = createHtmlElementTask(contentObj);
+	let contentDiv = createHtmlElementContent();
+	let checkbox = createHtmlElementCheckBox(contentDiv, contentObj);
+	taskDiv.appendChild(checkbox);
+	taskDiv.appendChild(contentDiv);
+	let contentInput = createHtmlElementContentInput(contentObj);
+	contentDiv.appendChild(contentInput);
+	let buttonsDiv = createHtmlElmentButtons(contentInput, taskDiv, tasksDiv);
+	// add input with task value into content div
+	taskDiv.appendChild(buttonsDiv);
+	tasksDiv.appendChild(taskDiv);
 
 }
 
+function displayAllTasksFromStorage(divTasks) {
+	util.removeAllChildNodes(divTasks);
+	//Load tasks to the screen
+	// ls.loadAllTasksFromStorageToPage();
+	let storedTasks = ls.loadStorageContent();
+	console.log('local storage tasks size: ' + storedTasks.length);
+	let tasksDiv = document.querySelector("#tasks");
+
+	storedTasks.forEach(element => {
+		let content = JSON.parse(element);
+		let taskDiv = createHtmlElementTask(content);
+		let contentDiv = createHtmlElementContent();
+		let checkbox = createHtmlElementCheckBox(contentDiv, content);
+
+		taskDiv.appendChild(checkbox);
+		taskDiv.appendChild(contentDiv);
+		let contentInput = createHtmlElementContentInput(content);
+		contentDiv.appendChild(contentInput);
+		let buttonsDiv = createHtmlElmentButtons(contentInput, taskDiv, tasksDiv);
+		// add input with task value into content div
+		taskDiv.appendChild(buttonsDiv);
+		tasksDiv.appendChild(taskDiv);
+	});
+
+}
+
+function createHtmlElementTask(jsonContent) {
+	const divTask = document.createElement('div');
+	divTask.id = jsonContent.id;
+	divTask.classList.add('task');
+	return divTask;
+}
+
+function createHtmlElementCheckBox(contentDiv, content) {
+	const task_input_check_el = document.createElement('input');
+	task_input_check_el.classList.add('cbox');
+	task_input_check_el.type = 'checkbox';
+	task_input_check_el.value = 'checkbox';
+	task_input_check_el.checked = content.completed;
+	if (content.completed) {
+		contentDiv.classList.add('strike');
+	}
+
+	task_input_check_el.addEventListener('change', (e) => {
+		let checked = task_input_check_el.checked
+		if (checked) {
+			contentDiv.classList.add('strike');
+		} else {
+			contentDiv.classList.remove('strike')
+		}
+		//update value in Storage
+		ls.updateItem(content.id, content.content, checked);
+
+	});
+	return task_input_check_el;
+}
+
+function createHtmlElementContentInput(jsonContent) {
+	// create input element with task
+	const task_input_el = document.createElement('input');
+	task_input_el.classList.add('text');
+	task_input_el.type = 'text';
+	task_input_el.name = 'todoItem';
+	task_input_el.id = 'todoItem';
+	task_input_el.value = jsonContent.content;
+	task_input_el.setAttribute('readonly', 'readonly');
+	return task_input_el;
+}
+
+function createHtmlElementContent() {
+	// create div for content
+	const task_content_el = document.createElement('div');
+	task_content_el.classList.add('content');
+	return task_content_el;
+}
+
+function createHtmlElmentButtons(task_input_el, divTask, divTasks) {
+	// create actions (buttons div)
+	const task_actions_el = document.createElement('div');
+	task_actions_el.classList.add('actions');
+	// edit button
+	const task_edit_el = document.createElement('button');
+	task_edit_el.classList.add('edit');
+	task_edit_el.innerText = 'Edit';
+	// Delete button
+	const task_delete_el = document.createElement('button');
+	task_delete_el.classList.add('delete');
+	task_delete_el.innerText = 'Delete';
+	// add to div with buttons
+	task_actions_el.appendChild(task_edit_el);
+	task_actions_el.appendChild(task_delete_el);
+
+	divTask.appendChild(task_actions_el);
+	divTasks.appendChild(divTask);
+
+	// Create event listener
+	task_edit_el.addEventListener('click', (e) => {
+		if (task_edit_el.innerText.toLowerCase() == "edit") {
+			task_edit_el.innerText = "Save";
+			task_input_el.removeAttribute("readonly");
+			task_input_el.focus();
+		} else {
+			task_edit_el.innerText = "Edit";
+			task_input_el.setAttribute("readonly", "readonly");
+			ls.updateItem(divTask.id, task_input_el.value);
+		}
+	});
+	task_delete_el.addEventListener('click', (e) => {
+		ls.removeFromStorage(divTask.id);
+		divTasks.removeChild(divTask);
+	});
+
+	return task_actions_el;
+}
